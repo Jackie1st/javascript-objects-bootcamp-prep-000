415f2ec4eb1ce252b5209ea0c165c1f811ed93b3
Jackie First <jackiejeep@gmail.com>
2018-03-29 21:48:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,11 +2,10 @@ var playlist = {
   artistName: 'songTitle'
 };
 function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle}
   playlist = {Slowdive: Alison,
     'My Bloody Valentine': Sometimes,
     'Phil Ochs': 'Here\'s to you Mississippi'
-  }
-  return playlist
 }
 function removeFromPlaylist(playlistObject, artistName) {
   delete playlist.artistName;
