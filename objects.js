8a343e604b442218036c49db756a9fa12429612a
Jackie First <jackiejeep@gmail.com>
2018-03-29 21:50:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
     'Phil Ochs': 'Here\'s to you Mississippi'
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist.Slowdive;
   return playlist;
 }
