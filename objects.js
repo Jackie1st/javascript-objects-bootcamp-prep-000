1fb9f5b66971f05138cd7814cfd2b9e778c63271
Jackie First <jackiejeep@gmail.com>
2018-03-29 21:38:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   artistName: 'songTitle'
 };
 function updatePlaylist(playlist, artistName, songTitle) {
-  
+  return Object.assign({}, artistName, )
 }
 function removeFromPlaylist(playlistObject, artistName) {
   delete playlist.artistName;
